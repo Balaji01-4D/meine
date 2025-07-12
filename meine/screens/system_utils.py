cf057c0a6386857eb99a43d900f823e97efee8fb
Balaji01-4D <j.balaji2468@gmail.com>
2025-07-12 22:58:33 +0530

    added a new dynamic widgets screen

--- meine/screens/system_utils.py
+++ meine/screens/system_utils.py
@@ -12,24 +12,16 @@ from meine.widgets.system_widget_provider import SystemWidgetProvider
 
     
 system_utils_functions = [
-    ("system_info", 1),         # SYSTEM
-    ("cpu_usage", 2),           # CPU
-    ("memory_info", 3),         # RAM Info
-    ("disk_usage", 4),          # DiskSpace
-    ("disk_details", 5),        # DiskInfo
-    ("running_processes", 6),   # Processes
-    ("network_interfaces", 7),  # NetWork
-    ("ip_info", 8),             # IP
-    ("battery_status", 9),      # Battery
-    ("current_user", 10),       # USER
-    ("current_time", 11),       # Time
-    ("home_directory", 12),     # HomeDir
-    ("current_directory", 13),  # GetCurrentDir
-    ("environment_vars", 14),   # ENV
-    ("file_info", 15),          # Info
-    ("terminate_process", 16),  # ProcessKill
-    ("reboot_system", 17),      # Reboot
-    ("shutdown_system", 18),    # ShutDown
+    ("system_info", 1),         
+    ("cpu_usage", 2),           
+    ("memory_info", 3),         
+    ("disk_details", 5),        
+    ("network_interfaces", 7),  
+    ("ip_info", 8),             
+    ("battery_status", 9),      
+    ("current_user", 10),       
+    ("environment_vars", 14),   
+
 ]
 
 
@@ -41,9 +33,7 @@ class SystemUtilScreen(ModalScreen):
         with Center():
             yield select 
 
-        with Center():
-
-            yield VerticalScroll(SystemWidgetProvider(id="system-utils-provider"),id="widget-container")
+        yield VerticalScroll(SystemWidgetProvider(id="system-utils-provider"),id="widget-container")
 
 
     def update_widget(self, function_id):
